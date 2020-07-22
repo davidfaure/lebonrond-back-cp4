c6e372758bfbde369cb3169cfe6f82b4bc887f5d
David Faure <aswelldavid@gmail.com>
2020-07-22 10:06:54 +0200

    authentication ok

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,14 @@ const express = require("express");
 const users = require('./users');
 const categories = require('./categories')
 const annonces = require('./annonces');
-// const auth = require('./auth');
+const auth = require('./auth');
 
 const router = express.Router();
 
 router.use("/users", users);
 router.use('/categories', categories)
 router.use('/annonces', annonces)
-// router.use('/auth', auth);
+router.use('/auth', auth);
 
 
 
